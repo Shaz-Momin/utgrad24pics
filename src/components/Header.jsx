fbaf6ebb70fd33c70dba0b5186207a78a27dbefb
Shaz Momin <shaz.momin@utexas.edu>
2024-05-20 03:38:57 -0500

    Improved SEO

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
         <div className="hero-section overflow-y-scroll bg-cover bg-fixed bg-center">
             <div className="bg-black h-full w-full bg-opacity-40">
                 <div className="text-white flex flex-col h-full w-full p-16 items-start justify-end tracking-widest overflow-hidden">
-                    <div className="font-serif text-3xl py-4">Shaz's Graduation Photos</div>
-                    <div className="font-normal uppercase">April 6, 2024</div>
+                    <h1 className="font-serif text-3xl py-4">Shaz's Graduation Photos</h1>
+                    <h3 className="font-light uppercase">April 6, 2024</h3>
                     <button className="arrow-scroll h-16 -mb-16 relative w-full" onClick={() => scrollTo({ top: innerHeight, behavior: "smooth"})}>
                         <div className="arrow"></div>
                         <div className="arrow"></div>
