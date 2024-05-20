7493ce3af3f995eab80de5ebdeed4c62883c95a7
Shaz Momin <shaz.momin@utexas.edu>
2024-05-20 16:26:10 -0500

    updated credits & images path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
 	return (
         <div className="hero-section overflow-y-scroll bg-cover bg-fixed bg-center">
             <div className="bg-black h-full w-full bg-opacity-40">
-                <div className="text-white flex flex-col h-full w-full p-16 items-start justify-end tracking-widest overflow-hidden">
+                <div className="text-white flex flex-col h-full w-full p-16 md:items-start justify-end tracking-widest overflow-hidden">
                     <h1 className="font-serif text-3xl py-4">Shaz's Graduation Photos</h1>
                     <h3 className="font-light uppercase">April 6, 2024</h3>
                     <button className="arrow-scroll h-16 -mb-16 relative w-full" onClick={() => scrollTo({ top: innerHeight, behavior: "smooth"})}>
